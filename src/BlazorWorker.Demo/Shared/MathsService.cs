136a9a60013d3463745c2d970aee4b5ad4772999
Tor KNUTSSON <Tewr@users.noreply.github.com>
2020-06-11 22:53:30 +0200

    OK for now

--- src/BlazorWorker.Demo/Shared/MathsService.cs
+++ src/BlazorWorker.Demo/Shared/MathsService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace BlazorWorker.Demo.Shared
 {
@@ -12,6 +14,7 @@ namespace BlazorWorker.Demo.Shared
     public class MathsService
     {
         public event EventHandler<int> Pi;
+        public event EventHandler<Tuple<int, double?>> PiCancellable;
 
         private IEnumerable<int> AlternatingSequence(int start = 0)
         {
@@ -46,6 +49,82 @@ namespace BlazorWorker.Demo.Shared
                     }
                     return x; })
                 .Sum(x => 1.0 / x));
+        }
+
+        private int idSource;
+        private Dictionary<int, CancellationTokenSource> cancellationTokens = new Dictionary<int, CancellationTokenSource>();
+
+        public int GetCancellationToken()
+        {
+            var nextId = ++idSource;
+            cancellationTokens.Add(nextId, new CancellationTokenSource());
+
+            return nextId;
+        }
+
+        public bool CancelCancellationToken(int id)
+        {
+            Console.WriteLine($"entering {nameof(CancelCancellationToken)} ");
+            if (cancellationTokens.TryGetValue(id, out var cancellationTokenSource))
+            {
+                cancellationTokenSource.Cancel();
+                var cancellationToken = cancellationTokenSource.Token;
+                Console.WriteLine($"{nameof(CancelCancellationToken)}: {nameof(cancellationToken.IsCancellationRequested)}={cancellationToken.IsCancellationRequested} {cancellationToken.GetHashCode()}");
+                return true;
+            }
+
+            return false;
+        }
+
+
+        public void EstimatePICancellable(int sumLength, int cancellationSourceId)
+        {
+            if (!cancellationTokens.TryGetValue(cancellationSourceId, out var cancellationTokenSource))
+            {
+                Console.WriteLine("Unknown cancellation source id", nameof(cancellationSourceId));
+                throw new ArgumentException("Unknown cancellation source id", nameof(cancellationSourceId));
+            }
+
+            Task.Run(async () => await BeginEstimatePI(sumLength, cancellationTokenSource.Token));
+        }
+
+        
+
+        private async Task BeginEstimatePI(int sumLength, CancellationToken cancellationToken)
+        {
+            try
+            {
+                Console.WriteLine($"entering {nameof(BeginEstimatePI)} ");
+
+                var lastReport = 0;
+                var i = 0;
+                var sum = 0d;
+                foreach (var item in AlternatingSequence().Take(sumLength))
+                {
+                    // Keep reporting events down a bit, serialization is expensive!
+                    var progressDelta = (Math.Abs(i - lastReport) / (double)sumLength) * 100;
+                    if (progressDelta > 3 || i >= sumLength - 1)
+                    {
+                        await Task.Delay(10);
+                        Console.WriteLine($"{nameof(BeginEstimatePI)}: {nameof(cancellationToken.IsCancellationRequested)}={cancellationToken.IsCancellationRequested} {cancellationToken.GetHashCode()}");
+                        cancellationToken.ThrowIfCancellationRequested();
+                        lastReport = i;
+                        Console.WriteLine($"invoking Pi event with arg {i}");
+                        PiCancellable?.Invoke(this, new Tuple<int, double?>(i, null));
+                    }
+
+                    sum += 1.0 / item;
+                    i++;
+                }
+
+                var result = 4 * sum;
+                PiCancellable?.Invoke(this, new Tuple<int, double?>(i, result));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{nameof(BeginEstimatePI)} {e}");
+                throw;
+            }
         }
 
         public double EstimatePISlice(int sumStart, int sumLength)
